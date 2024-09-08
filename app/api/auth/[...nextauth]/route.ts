069b443f15c74ea0f2ba41b915988f71f148a609
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-09-07 21:22:53 -0500

    Fix Initial Deployment Conflicts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,34 @@
-import { AuthOptions } from "next-auth";
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler: AuthOptions = NextAuth({
-    providers: [    // CONSIDER HERE MFA FLAG FOR SECOND AUTH STEP
-
-        CredentialsProvider({
-            name: 'credentials',
-            credentials: {
-                email: { label: 'Correo electrónico', type: "text", placeholder: "ejemplo@email.com"},
-                password: { label: 'Contraseña', type: "password"},
-            },
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            async authorize(credentials, req) {
-                const { email, password } = credentials || {};
-                if (!email || !password) {
-                    return null;
-                }
-                // TODO ADD AXIOS FETCH HERE TO BACKEND.
-                return { user: "Hello", id: "1", email: "hellomoto@email.com"};
-            },
-        })
-    ],
-    callbacks:  {}, // TODO IMPLEMENT HERE TYPED SESSION AND JWT BASED ON DATA STRUCT,
+const handler = NextAuth({
+  providers: [
     // CONSIDER HERE MFA FLAG FOR SECOND AUTH STEP
-    secret: process.env.NEXTAUTH_SECRET,
+
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Correo electrónico",
+          type: "text",
+          placeholder: "ejemplo@email.com",
+        },
+        password: { label: "Contraseña", type: "password" },
+      },
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      async authorize(credentials, req) {
+        const { email, password } = credentials || {};
+        if (!email || !password) {
+          return null;
+        }
+        // TODO ADD AXIOS FETCH HERE TO BACKEND.
+        return { user: "Hello", id: "1", email: "hellomoto@email.com" };
+      },
+    }),
+  ],
+  callbacks: {}, // TODO IMPLEMENT HERE TYPED SESSION AND JWT BASED ON DATA STRUCT,
+  // CONSIDER HERE MFA FLAG FOR SECOND AUTH STEP
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
