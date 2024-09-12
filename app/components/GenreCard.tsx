1771384353a588b8d35edf3f8d223a6aac0365ce
Mateo Guerrero <140415694+MateoGuerreroE@users.noreply.github.com>
2024-09-12 09:40:55 -0500

    [Depolyment] Fix Linting Issues

--- app/components/GenreCard.tsx
+++ app/components/GenreCard.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { Card, CardBody } from "@nextui-org/react";
 import React from "react";
 
 type CardProps = { genreId: string; name: string };
 
 export default function GenreCard({ genreId, name }: CardProps) {
+  console.log(genreId);
   return (
     <li className="p-0.5 font-aksara text-lg pl-3 hover:bg-[#4b4b4b] hover:cursor-pointer">
       {name}
